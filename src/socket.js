70ceabe98f481690c3473102f80eed74af4be69c
Anish-0411 <govindusaianish@gmail.com>
2025-08-01 16:35:33 +0530

    fix: CORS socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,23 +1,37 @@
+// import { io } from 'socket.io-client';
+
+
+
+// const socket = io(process.env.REACT_APP_API_URL, {
+//   transports: ["websocket", "polling"],
+//   secure: true,
+//   withCredentials: true,
+//   reconnection: true,
+// });
+
+
+// socket.on('connect_error', (err) => {
+//   console.error("❌ WebSocket failed:", err.message);
+// });
+
+// export default socket;
+
+
 import { io } from 'socket.io-client';
 
-
-// const API = process.env.REACT_APP_API_URL;
-
-// const socket = io(API, {
-//   transports: ["websocket"],
-//   secure: true,
-// });
 const socket = io(process.env.REACT_APP_API_URL, {
-  transports: ["websocket", "polling"],
-  secure: true,
+  transports: ['polling', 'websocket'], // polling first
   withCredentials: true,
   reconnection: true,
+  secure: true,
 });
 
+socket.on('connect', () => {
+  console.log('✅ Socket connected:', socket.id);
+});
 
 socket.on('connect_error', (err) => {
-  console.error("❌ WebSocket failed:", err.message);
+  console.error('❌ WebSocket failed:', err.message);
 });
 
 export default socket;
-
